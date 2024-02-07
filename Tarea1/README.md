bed5d4a416cbdb4064e70c3c2954a545567ea8ea
joseCarlosAc <jose.acevesgon@gmail.com>
2024-02-06 22:10:53 -0600

    tarea 1

---
+++ Tarea1/README.md
@@ -0,0 +1,22 @@
+# Project Name
+
+## Description
+
+Provide a brief description of your project here.
+
+## Prerequisites
+
+-   Node.js (v20.11.0)
+-   npm (v10.2.4)
+
+## Installation
+
+1. Clone the repository:
+
+```sh
+npm install
+
+npm start
+
+npm run dev
+```
---
+++ Tarea1/src/routes/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const authRoutes = require("./auth");
+const usersRoutes = require("./users");
+
+router.use("/auth", authRoutes);
+router.use("/users", usersRoutes);
+
+// Define your endpoints here
+router.get("/", (req, res) => {
+  res.send("routes works");
+});
+
+module.exports = router;
