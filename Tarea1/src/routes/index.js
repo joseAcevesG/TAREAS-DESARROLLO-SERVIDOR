32c10e5df359497abed0f7e7ad586a7ebef56afb
joseCarlosAc <jose.acevesgon@gmail.com>
2024-02-07 22:25:53 -0600

    fix of routes

--- Tarea1/src/routes/index.js
+++ Tarea1/src/routes/index.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const authRoutes = require("./auth");
 const usersRoutes = require("./users");
 
-router.use("/auth", authRoutes);
 router.use("/users", usersRoutes);
 
 // Define your endpoints here
 router.get("/", (req, res) => {
-  res.send("routes works");
+	res.send("routes works");
 });
 
 module.exports = router;
