5b00ef1cb3fac478859f2702d23ad0b167b9ab04
“joseAcevesG” <jose.aceves@iteso.mx>
2024-03-07 13:32:32 -0600

    news routes added to the API

--- Tarea4/src/routes/index.js
+++ Tarea4/src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const authRoutes = require("./auth");
+const newsRoutes = require("./news");
 
 router.use("/auth", authRoutes);
+router.use("/news", newsRoutes);
 
 module.exports = router;
