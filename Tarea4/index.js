877ffd615cf41a21bbf7ce46c1b23ba5dcddafd1
joseAcevesG <119601853+joseAcevesG@users.noreply.github.com>
2024-03-07 22:41:07 +0000

    config for the handlebars and the assets route

--- Tarea4/index.js
+++ Tarea4/index.js
@@ -2,13 +2,19 @@ const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const routes = require("./src/routes");
+const path = require("path");
+const { engine } = require("express-handlebars");
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.use(express.json());
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
 
+app.use(express.json());
+app.use("/assets", express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 async function start() {
