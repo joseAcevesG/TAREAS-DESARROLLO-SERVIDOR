d4b44d295cd1985c4e4a37fc90487e22f22be8c6
“joseAcevesG” <jose.aceves@iteso.mx>
2024-03-06 22:45:30 -0600

    model to manage database

---
+++ Tarea4/src/models/user.js
@@ -0,0 +1,19 @@
+const { Shema, model } = require("mongoose");
+
+const schema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+});
+
+module.exports = model("users", schema);
