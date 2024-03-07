fc6861d038bf10e0257afbe0eaff3f2b9cc9f69e
joseAcevesG <119601853+joseAcevesG@users.noreply.github.com>
2024-03-07 22:36:53 +0000

    route for the home page

--- Tarea4/src/routes/index.js
+++ Tarea4/src/routes/index.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 const authRoutes = require("./auth");
 const newsRoutes = require("./news");
 
+router.get("/", (req, res) => {
+	res.render("home");
+});
 router.use("/auth", authRoutes);
 router.use("/news", newsRoutes);
 
