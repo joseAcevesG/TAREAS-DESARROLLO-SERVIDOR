7dd1e74d0881fea5e8b92148e72d701ee0fb519c
joseAcevesG <119601853+joseAcevesG@users.noreply.github.com>
2024-03-08 16:12:19 +0000

    fshow log out or log in correctly

--- Tarea4/public/scripts/script.js
+++ Tarea4/public/scripts/script.js
@@ -1,18 +1,37 @@
 const signUpModal = document.querySelector("#singUp");
+
+function isLogged() {
+	const signUpButton = document.querySelector("#signUpButton");
+	const logInButton = document.querySelector("#logInButton");
+	const logOutButton = document.querySelector("#logOut");
+
+	if (localStorage.getItem("token")) {
+		signUpButton.style.display = "none";
+		logInButton.style.display = "none";
+		logOutButton.style.display = "block";
+	} else {
+		signUpButton.style.display = "inline";
+		logInButton.style.display = "inline";
+		logOutButton.style.display = "none";
+	}
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+	isLogged();
+
 	const saveButton = document.querySelector(
 		"#singUp .modal-footer .btn-primary"
 	);
 
 	saveButton.addEventListener("click", function () {
-		const name = document.querySelector("#singUp #name").value;
-		const email = document.querySelector("#singUp #email").value;
-		const password = document.querySelector("#singUp #password").value;
+		const name = document.querySelector("#singUp #name");
+		const email = document.querySelector("#singUp #email");
+		const password = document.querySelector("#singUp #password");
 
 		const formData = {
-			name: name,
-			email: email,
-			password: password,
+			name: name.value,
+			email: email.value,
+			password: password.value,
 		};
 
 		fetch("auth/signUp", {
@@ -28,6 +47,10 @@ document.addEventListener("DOMContentLoaded", function () {
 						throw new Error(text);
 					});
 				}
+				$("#singUp").modal("hide");
+				name.value = "";
+				email.value = "";
+				password.value = "";
 				return response.text();
 			})
 			.then((data) => {
@@ -49,11 +72,60 @@ document.addEventListener("DOMContentLoaded", function () {
 			})
 			.then((data) => {
 				localStorage.setItem("token", data.token);
+				isLogged();
+
 				console.log(data.token);
 			})
 			.catch((error) => {
 				console.error(error.message);
 				alert(error.message);
 			});
+	});
+
+	const logInButtonModal = document.querySelector(
+		"#logIn .modal-footer .btn-primary"
+	);
+
+	logInButtonModal.addEventListener("click", function () {
+		const email = document.querySelector("#logIn #email");
+		const password = document.querySelector("#logIn #password");
+
+		const formData = {
+			email: email.value,
+			password: password.value,
+		};
+
+		fetch("auth/logIn", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(formData),
+		})
+			.then((response) => {
+				if (!response.ok) {
+					return response.text().then((text) => {
+						throw new Error(text);
+					});
+				}
+				$("#logIn").modal("hide");
+				email.value = "";
+				password.value = "";
+				return response.json();
+			})
+			.then((data) => {
+				localStorage.setItem("token", data.token);
+				isLogged();
+			})
+			.catch((error) => {
+				console.error(error.message);
+				alert(error.message);
+			});
+	});
+
+	const logOutButton = document.querySelector("#logOut");
+	logOutButton.addEventListener("click", function () {
+		localStorage.removeItem("token");
+		isLogged();
 	});
 });
